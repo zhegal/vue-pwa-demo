e760398cb78a9693927dda92c1a058e2d621b434
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-09-04 21:48:03 +0300

    2. vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: { globPatterns: ["**/*.{html,css,js,ico,png,svg}"] },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
