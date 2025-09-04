08559833a12f6733ca20974da9e52d84231d2e84
Evgeniy Galkovskiy <zhegal@gmail.com>
2025-09-04 22:25:33 +0300

    4. text in app vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hello World!</h1>
+  <h1>Oh Wow! What a cool app!!!</h1>
+  <h2>Please use it every day!!!</h2>
 </template>
 
 <style scoped></style>
